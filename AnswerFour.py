6f3e5d3b71d13dc95c30d4d5d92bfbfe6129435b
RavidRomy <ravid.romy@gmail.com>
2021-04-28 03:11:28 +0300

    Answer for question 4 done

---
+++ AnswerFour.py
@@ -0,0 +1,37 @@
+################################
+# Python Answer for Question 4 #
+# ====== BY HEISENBERG ======= #
+###### . - - - - - - - - . #####
+###### |                 | #####
+#### ----= ___________ =---- ###
+######## ___        ___ ########
+#######_| ___) == (___ |_#######
+#########\     ***     /########
+##########\   * = *   /#########
+###########\  *****  /##########
+################################
+
+
+def check_id(idnum):
+    if len(idnum) != 9:
+        return False
+    list()
+    try:
+        idvalue = list(map(int, idnum))
+    except:
+        return False
+    counter = 0
+    for i in range(9):
+        idvalue[i] *= (i % 2) + 1
+        if idvalue[i] > 9:
+            idvalue[i] -= 9
+        counter += idvalue[i]
+    if counter % 10 == 0:
+        return True
+    else:
+        return False
+
+
+print(check_id("543700421"))
+print(check_id("322248097"))
+print(check_id("322248093"))
