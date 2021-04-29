8c19439444b075faf699dc0b7018ea9d516cc9c3
RavidRomy <ravid.romy@gmail.com>
2021-04-29 06:02:34 +0300

    Finished answer 6

--- AnswerSix.py
+++ AnswerSix.py
@@ -10,5 +10,24 @@
 ##########\   * = *   /#########
 ###########\  *****  /##########
 ################################
+import time
 
-main
+
+def memoize(function):
+    memo = []
+
+    def wrapper(*args):
+        if args in memo:
+            return memo[args]
+        else:
+            vector = function(*args)
+            memo[args] = vector
+            return vector
+
+    return wrapper
+
+
+@memoize
+def multi(x):
+    time.sleep(6)
+    return x*2
